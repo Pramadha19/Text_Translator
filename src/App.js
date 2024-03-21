c3cacb58e0657d387d84caf5a50de0ace12c2411
Pramadha Yaramakala <pramadhayaramakala@Pramadhas-MacBook-Pro.local>
2024-03-21 23:46:45 +0530

    text translator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Translator from './components/Translator';
+import './App.css'; 
+
+function App() { 
+return ( 
+	<div className="App"> 
+	<Translator/> 
+	</div> 
+); 
+} 
+
+export default App; 
